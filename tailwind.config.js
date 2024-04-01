452905e4b2c979d33e0bbd541dedab849c35e604
cursefroge <curse@curseforged.dev>
2024-04-01 11:10:44 -0400

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      "poop": "#7a5901",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
